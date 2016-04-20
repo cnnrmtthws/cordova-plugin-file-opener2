1fc01725ffab11cf9693bccd4a82d65159825f37
unknown <joshr@apa-joshr.apadmi.com>
2016-04-20 16:46:05 +0100

    Use presentOpenInMenuFromRect rather than presentOptionsMenuFromRect.

--- src/ios/FileOpener2.m
+++ src/ios/FileOpener2.m
@@ -35,7 +35,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
     if (!uti || (NSNull*)uti == [NSNull null]) {
         NSArray *dotParts = [path componentsSeparatedByString:@"."];
         NSString *fileExt = [dotParts lastObject];
-        
+
         uti = (__bridge NSString *)UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, (__bridge CFStringRef)fileExt, NULL);
     }
 
@@ -44,9 +44,9 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
     dispatch_async(dispatch_get_main_queue(), ^{
         // TODO: test if this is a URI or a path
         NSURL *fileURL = [NSURL URLWithString:path];
-        
+
         localFile = fileURL.path;
-        
+
         NSLog(@"looking for file at %@", fileURL);
         NSFileManager *fm = [NSFileManager defaultManager];
         if(![fm fileExistsAtPath:localFile]) {
@@ -64,7 +64,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
         CGRect rect = CGRectMake(0, 0, 1000.0f, 150.0f);
         CDVPluginResult* pluginResult = nil;
-        BOOL wasOpened = [self.controller presentOptionsMenuFromRect:rect inView:cont.view animated:NO];
+        BOOL wasOpened = [self.controller presentOpenInMenuFromRect:rect inView:cont.view animated:NO];
 
         if(wasOpened) {
             pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: @""];
